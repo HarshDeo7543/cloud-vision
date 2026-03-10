9e1fd7b7300aa71a99d106e7a86fdf4d6919b668
Harsh Deo <harshdeo5142@gmail.com>
2026-03-10 11:43:23 +0530

    EC2 server added in code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import React, { useState, useRef } from 'react';
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
-
-// const API_URL = 'http://13.50.235.92/';
+// const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = (process.env.REACT_APP_API_URL || 'http://13.50.235.92' || 'http://localhost:5000').replace(/\/$/, '');
 
 function App() {
   const [file, setFile] = useState(null);
